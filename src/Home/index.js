09393444f41bfb415e973f4af40a39e57ec5c8ea
Aaron <nzoLogic@gmail.com>
2019-05-22 13:51:56 -0400

    launch darkly stuff

---
+++ src/Home/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from '../logo.svg';
+import { withLDConsumer } from 'launchdarkly-react-client-sdk';
+
+
+const style1 = {
+  backgroundColor: '#282c34',
+  minHeight: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: 'calc(10px + 2vmin)',
+  color: 'white',
+}
+const style2 = {
+  backgroundColor: 'blue',
+  minHeight: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: 'calc(10px + 2vmin)',
+  color: 'white',
+}
+
+const Home = (props) => (
+  <header style={props.flags.steeleLearning ? style2 : style1}>
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+)
+
+export default withLDConsumer()(Home)
